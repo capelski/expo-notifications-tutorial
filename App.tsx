a1b9a141b73d5ce71aad9475a471e35e45d0248d
Carles Capellas <capellas.carles@gmail.com>
2021-08-02 17:54:26 +0200

    chore: improve notifications error handling

--- App.tsx
+++ App.tsx
@@ -12,33 +12,39 @@ Notifications.setNotificationHandler({
     })
 });
 
-const registerForPushNotificationsAsync = async () => {
-    let token;
-    if (Constants.isDevice) {
-        const { status: existingStatus } = await Notifications.getPermissionsAsync();
-        let finalStatus = existingStatus;
-        if (existingStatus !== 'granted') {
-            const { status } = await Notifications.requestPermissionsAsync();
-            finalStatus = status;
-        }
-        if (finalStatus !== 'granted') {
-            throw 'Failed to get push token for push notification!';
-        }
-        token = (await Notifications.getExpoPushTokenAsync()).data;
-    } else {
-        throw 'Must use physical device for Push Notifications';
+const registerForPushNotificationsAsync = () => {
+    if (!Constants.isDevice) {
+        return Promise.reject('Must use physical device for Push Notifications');
     }
 
-    if (Platform.OS === 'android') {
-        Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C'
-        });
-    }
+    try {
+        return Notifications.getPermissionsAsync()
+            .then((statusResult) => {
+                return statusResult.status !== 'granted'
+                    ? Notifications.requestPermissionsAsync()
+                    : statusResult;
+            })
+            .then((statusResult) => {
+                if (statusResult.status !== 'granted') {
+                    throw 'Failed to get push token for push notification!';
+                }
+                return Notifications.getExpoPushTokenAsync();
+            })
+            .then((tokenData) => {
+                if (Platform.OS === 'android') {
+                    Notifications.setNotificationChannelAsync('default', {
+                        name: 'default',
+                        importance: Notifications.AndroidImportance.MAX,
+                        vibrationPattern: [0, 250, 250, 250],
+                        lightColor: '#FF231F7C'
+                    });
+                }
 
-    return token;
+                return tokenData.data;
+            });
+    } catch (error) {
+        return Promise.reject("Couldn't check notifications permissions");
+    }
 };
 
 const schedulePushNotification = () =>
