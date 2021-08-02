3ee4f2281b237602ebc281622d83e5c955de9c03
Carles Capellas <capellas.carles@gmail.com>
2021-08-02 15:55:25 +0200

    feat: setup notifications

--- App.tsx
+++ App.tsx
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Subscription } from '@unimodules/core';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Platform, Text, View } from 'react-native';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false
+    })
+});
+
+const registerForPushNotificationsAsync = async () => {
+    let token;
+    if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C'
+        });
+    }
+
+    return token;
+};
+
+const schedulePushNotification = () =>
+    Notifications.scheduleNotificationAsync({
+        content: {
+            title: "You've got mail! 📬",
+            body: 'Here is the notification body',
+            data: { data: 'goes here' }
+        },
+        trigger: { seconds: 2 }
+    });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [expoPushToken, setExpoPushToken] = useState<string>();
+    const [notification, setNotification] = useState<Notifications.Notification>();
+    const notificationListener = useRef<Subscription>();
+    const responseListener = useRef<Subscription>();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(setExpoPushToken);
+
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener(setNotification);
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(
+            (response) => {
+                setNotification(response.notification);
+            }
+        );
+
+        return () => {
+            notificationListener.current &&
+                Notifications.removeNotificationSubscription(notificationListener.current);
+            responseListener.current &&
+                Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'space-around'
+            }}
+        >
+            <View>
+                <Text>Your expo push token:</Text>
+                <Text selectable={true}>{expoPushToken}</Text>
+            </View>
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Title: {notification && notification.request.content.title} </Text>
+                <Text>Body: {notification && notification.request.content.body}</Text>
+                <Text>
+                    Data: {notification && JSON.stringify(notification.request.content.data)}
+                </Text>
+            </View>
+            <Button title="Press to schedule a notification" onPress={schedulePushNotification} />
+        </View>
+    );
+}
